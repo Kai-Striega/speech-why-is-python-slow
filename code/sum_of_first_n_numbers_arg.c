eac7750ad85149121be2533862b4c66e05dff077
Kai Striega <kaistriega@gmail.com>
2023-10-09 15:38:01 +0800

    Move lines to make my OCD happy

--- code/sum_of_first_n_numbers_arg.c
+++ code/sum_of_first_n_numbers_arg.c
@@ -6,8 +6,8 @@
 // To see asm (-O0) https://godbolt.org/z/8Y498c3hs
 int main(int argc, const char *argv[]) {
     char *end;
-    uint64_t total = 0;
     uint64_t limit = (uint64_t) strtoull(argv[1], &end, 10);
+    uint64_t total = 0;
     for (uint64_t i = 0; i < limit; ++i) {
         total += i;
     }
