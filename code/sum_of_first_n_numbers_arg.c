bfd46c83372cf8fca9831e9c07ab0428b17e4a37
Kai Striega <kaistriega@gmail.com>
2023-10-09 15:33:39 +0800

    Add arg parsing to c version

--- code/sum_of_first_n_numbers_arg.c
+++ code/sum_of_first_n_numbers_arg.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <inttypes.h>
 
-// To see asm (-O3) https://godbolt.org/z/fKT4nenTv
-// To see asm (-O0) https://godbolt.org/z/PPzeKcE6r
+// To see asm (-O3) https://godbolt.org/z/4xjqYaY73
+// To see asm (-O0) https://godbolt.org/z/8Y498c3hs
 int main(int argc, const char *argv[]) {
+    char *end;
     uint64_t total = 0;
-    for (uint64_t i = 0; i < argc; ++i) {
+    uint64_t limit = (uint64_t) strtoull(argv[1], &end, 10);
+    for (uint64_t i = 0; i < limit; ++i) {
         total += i;
     }
     printf("total=%" PRIu64 "\n", total);
